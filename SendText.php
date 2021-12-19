a37edf5c3c1d8b68b805e5644edc336f236ef126
seasunrise22 <seasunrise22@gmail.com>
2021-12-19 23:33:39 +0900

    php로 데이터가 넘어가지 않는 문제 발생. 유니티 버그...?

--- SendText.php
+++ SendText.php
@@ -1,28 +1,42 @@
 <?php
-date_default_timezone_set('Asia/Seoul');
-$servername = "localhost";
-$username = "root";
-$password = "root";
-$dbname = "leenotepadappdb";
+
+// date_default_timezone_set('Asia/Seoul');
+// $servername = "localhost";
+// $username = "root";
+// $password = "root";
+// $dbname = "leenotepadappdb";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+// $conn = new mysqli($servername, $username, $password, $dbname);
+// // Check connection
+// if ($conn->connect_error) {
+//   die("Connection failed: " . $conn->connect_error);
+// }
+
+if(isset($_POST["num"]))
+{
+  $num = $_POST["num"];
+  echo "Set num. Num is... $num";
+}
+else
+{
+  echo "Not set num";
 }
 
-$datetime = date('Y-m-d H:i:s');
-$memotext = $_POST["text"];
+// $memotext = $_POST["text"];
+// $datetime = date('Y-m-d H:i:s');
+// echo "num : $num<br> 
+// memotext : $memotext<br> 
+// datetime : $datetime";
 
-$sql = "INSERT INTO memo_normal (inputtime, memotext)
-        VALUES ('$datetime', '$memotext')";
+// $sql = "INSERT INTO memo_normal (inputtime, memotext)
+//         VALUES ('$datetime', '$memotext')";
 
-if ($conn->query($sql) === TRUE) {
-  echo "New record created successfully";
-} else {
-  echo "Error: " . $sql . "<br>" . $conn->error;
-}
+// if ($conn->query($sql) === TRUE) {
+//   echo "New record created successfully";
+// } else {
+//   echo "Error: " . $sql . "<br>" . $conn->error;
+// }
 
-$conn->close();
+// $conn->close();
 ?>
